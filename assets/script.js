1cc53c60ce0d4be401e66ccf26d0912915f097a2
chooj743 <chooj743@newschool.edu>
2020-09-20 20:36:26 +0800

    .

--- assets/script.js
+++ assets/script.js
@@ -6,9 +6,10 @@ for (i = 0; i < coll.length; i++) {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
-      content.style.display = "none";
     } else {
       content.style.display = "block";
     }
   });
 }
+
+
